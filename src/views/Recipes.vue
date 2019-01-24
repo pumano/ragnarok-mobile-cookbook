c6db1d9427fecbfe3645e0726536c176dc86f1c7
pumano <pumano@gmail.com>
2019-01-24 19:46:50 +0300

    feat(recipes): add recipes

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
     <template>
-      <v-data-table :headers="headers" :items="rows" class="elevation-1">
+         <v-tabs fixed-tabs v-model="tabs">
+    <v-tab>
+      Luxury Worktop
+    </v-tab>
+    <v-tab>
+      Wild Life BBQ
+    </v-tab>
+    <v-tab>
+      Pressure Cooker
+    </v-tab>
+    <v-tab>
+      Beverage Stall
+    </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tabs" class="white elevation-1">
+      <v-tab-item>
+        <v-data-table :headers="headers" :items="rows" class="elevation-1">
         <template slot="items" slot-scope="props">
           <td class="text-xs-center">
             <h3>{{ props.item.name }}</h3>
@@ -22,6 +39,10 @@
           </td>
         </template>
       </v-data-table>
+      </v-tab-item>
+    </v-tabs-items>
+
+      
     </template>
   </div>
 </template>
@@ -51,13 +72,14 @@ export default class Recipes extends Vue {
   ];
 
   rows: Recipe[] = [];
+  tabs = 0;
 
   mounted() {
-    this.recipesData();
+    this.recipesData(this.adoptType(this.tabs));
   }
 
   // computed
-  recipesData() {
+  recipesData(type: string) {
     recipes.forEach(element => {
       if (element.ingredients) {
         element.ingredients = element.ingredients.map(ingredient =>
@@ -65,9 +87,30 @@ export default class Recipes extends Vue {
         );
       }
     });
-    this.rows = recipes;
+    const _recipes = recipes.filter(recipe => recipe.type === type);
+    this.rows = _recipes;
     console.log(this.rows);
     return recipes;
+  }
+
+  adoptType(tabId: number) {
+    switch(tabId) {
+      case 0: { 
+      return 'Luxury Worktop';
+      }
+      case 1: {
+        return 'Wild Life BBQ';
+      }
+      case 2: {
+        return 'Pressure Cooker';
+      }
+      case 3: {
+        return 'Beverage Stall';
+      }
+      default: {
+        return 'Luxury Worktop';
+      }
+    }
   }
 }
 </script>
