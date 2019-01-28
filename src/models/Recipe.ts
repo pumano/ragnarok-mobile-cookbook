90f4cb266d134551c7d21454e11efb01303c5ce1
pumano <pumano@gmail.com>
2019-01-28 16:39:30 +0300

    fix(recipes): fix model

--- src/models/Recipe.ts
+++ src/models/Recipe.ts
@@ -3,8 +3,8 @@ import { Ingredient } from './Ingredient';
 export class Recipe {
     constructor(
         public id?: number,
-        readonly name: string,
-        readonly description: string,
+        readonly name?: string,
+        readonly description?: string,
         readonly image?: string,
         readonly type?: 'Luxury Worktop' | 'Wild Life BBQ' | 'Pressure Cooker' | 'Beverage Stall',
         readonly stars?: number,
